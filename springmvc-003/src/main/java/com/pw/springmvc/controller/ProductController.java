1b9b76e7de746b30df4ba133d7b4b1086e2c1a40
xm <xmli@163.com>
2024-08-05 23:48:49 +0800

    03-springmvc-003

---
+++ springmvc-003/src/main/java/com/pw/springmvc/controller/ProductController.java
@@ -0,0 +1,14 @@
+package com.pw.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ProductController {
+
+    @RequestMapping("/product/detail")
+    public String productDetail(){
+        return "product_detail";
+    }
+
+}
