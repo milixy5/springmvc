ae916b5c1a18a1fbe5965ee1c10c211d46c01570
xm <xmli@163.com>
2024-09-01 11:48:01 +0800

    03-springmvc-003

--- springmvc-003/src/main/java/com/pw/springmvc/controller/IndexController.java
+++ springmvc-003/src/main/java/com/pw/springmvc/controller/IndexController.java
@@ -1,7 +1,9 @@
 package com.pw.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class IndexController {
@@ -9,5 +11,17 @@ public class IndexController {
     @RequestMapping("/")
     public String index(){
         return "index";
+    }
+
+    @RequestMapping("/login/{username}/{password}")
+    public String testRestful(@PathVariable("username") String username, @PathVariable("password") String password){
+        System.out.println("username: "+username+" "+"password: "+password);
+        return "ok";
+    }
+
+    @RequestMapping(value = "/user/login",method = RequestMethod.POST)
+    public String userLogin(){
+        System.out.println("正在处理登录的业务……");
+        return "ok";
     }
 }
--- springmvc-003/src/main/java/com/pw/springmvc/controller/ProductController.java
+++ springmvc-003/src/main/java/com/pw/springmvc/controller/ProductController.java
@@ -3,12 +3,18 @@ package com.pw.springmvc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/product")
 @Controller
 public class ProductController {
 
-    @RequestMapping("/product/detail")
+//    @RequestMapping("/product/detail")
+    @RequestMapping("/detail")
     public String productDetail(){
         return "product_detail";
     }
 
+    @RequestMapping("/x*z")
+    public String test(){
+        return "ok";
+    }
 }
